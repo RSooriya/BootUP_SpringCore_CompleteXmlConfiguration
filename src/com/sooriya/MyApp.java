174cd0fbc548eeeb01a97571794d3463c0745ac5
RSooriya <soormo06@gmail.com>
2020-07-04 18:15:51 +0530

    Comments added

--- src/com/sooriya/MyApp.java
+++ src/com/sooriya/MyApp.java
@@ -5,14 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyApp {
 
 	public static void main(String[] args) {
+	   // creating spring container and loading configuration file into container
        ClassPathXmlApplicationContext context = new
     		   ClassPathXmlApplicationContext("applicationContext.xml");
+       // retrieving beans from container
        Coach theCoach = context.getBean("myCoach",Coach.class);
        Coach alphaCoach = context.getBean("myCoach",Coach.class);
+       //calling methods on bean
        System.out.println(theCoach.getDailyWorkout());
        System.out.println(theCoach.getDailyFortune());
        System.out.println(theCoach.getDailyWorkoutTime());
        System.out.println(theCoach.getDetails());
+       // checking for scope 
        System.out.println("Singleton " + (theCoach==alphaCoach));
        context.close();
 	}
